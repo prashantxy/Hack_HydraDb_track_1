006ed3d565050506537ca93d6c28f3c8650997e8
Prashant Dubey <pdubey1924@gmail.com>
2026-08-19 01:39:11 +0530

    fixes

--- track01-backend/src/ontology/graph-writer.test.ts
+++ track01-backend/src/ontology/graph-writer.test.ts
@@ -1,31 +1,42 @@
-import { HydraClient } from "../core/hydra-client";
-import { OntologyGraphWriter } from "./graph-writer";
+import {
+  HydraClient,
+} from "../core/hydra-client";
+
+import {
+  OntologyGraphWriter,
+} from "./graph-writer";
 
 const client = new HydraClient();
 
-const writer = new OntologyGraphWriter(
-  client,
-);
+const writer =
+  new OntologyGraphWriter(client);
 
 const entity = {
   id: "person:test-001",
+
   type: "person" as const,
+
   name: "Soham Ratnaparkhi",
+
   aliases: [
     "Sam",
     "@soham",
   ],
+
   sourceIds: [
     {
       source: "slack",
       id: "U123",
     },
   ],
+
   attributes: {},
 };
 
-await writer.upsertEntity(entity);
+await writer.createEntity(entity);
 
 console.log(
   "✓ Entity written to HydraDB",
 );
+
+await client.close();
